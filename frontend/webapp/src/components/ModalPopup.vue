69a689d251f297fec913622195da5a550e792f98
Jakub Olszewski <olszewskikuba17@gmail.com>
2023-05-27 02:56:40 +0200

    DZIALA KURDEEEEE!!11!1!

--- frontend/webapp/src/components/ModalPopup.vue
+++ frontend/webapp/src/components/ModalPopup.vue
@@ -1,60 +1,20 @@
 <template>
-  <div>
-    <div class="container">
-      <video
-        ref="videoElement"
-        class="video-js"
-        preload="auto"
-        data-setup='{}'
-      ></video>
-    </div>
-  </div>
+  <video ref="video" width="100%" height="640" controls></video>
 </template>
 
 <script>
-import videojs from 'video.js';
-import '@videojs/http-streaming';
+import Hls from 'hls.js';
 
 export default {
   mounted() {
-    this.$nextTick(() => {
-      const videoElement = this.$refs.videoElement;
-
-      const player = videojs(videoElement, {
-        controlBar: {
-          playToggle: false,
-        },
-        userActions: {
-          hotkeys: false,
-          doubleClick: false,
-          fullscreenToggle: false,
-          contextMenu: false,
-          externalTracks: false,
-          keyboard: false,
-          pictureInPicture: false,
-          subtitlesButton: false,
-        },
-      });
-
-      // Dodaj zdarzenie click do odtwarzacza wideo
-      player.on('click', () => {
-        player.play();
-      });
-
-      player.src({
-        src: 'https://3bcc7697a17c.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.437790362083.channel.dnY029lLbZNG.m3u8',
-        type: 'application/x-mpegURL',
-      });
-
-      player.play();
+    let hls = new Hls();
+    let stream = "https://3bcc7697a17c.eu-central-1.playback.live-video.net/api/video/v1/eu-central-1.437790362083.channel.dnY029lLbZNG.m3u8";
+    let video = this.$refs["video"];
+    hls.loadSource(stream);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play();
     });
   },
 };
 </script>
-
-<style>
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-</style>
